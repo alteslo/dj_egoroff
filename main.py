d6b8cd553d1d904c0c7454f4aac8e45986556fb3
Teslenko A.A.(work) <alteslo31@gmail.com>
2022-01-17 18:47:36 +0300

    new function in main

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+url = input()
+if 'www.imdb.com/title' not in url:
+    print('Invalid movie page!')
+else:
+    movi_description = {}
+    response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'}).content
+    soup = BeautifulSoup(response, 'html.parser')
+
+    h_link = soup.find('h1', "TitleHeader__TitleText-sc-1wu6n3d-0 gxLYZW")
+    movi_description["title"] = h_link.text
+
+    h_link = soup.find('span', "GenresAndPlot__TextContainerBreakpointXL-sc-cum89p-2 eqbKRZ")
+    movi_description["description"] = h_link.text
+
+    print(movi_description)
