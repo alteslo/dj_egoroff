388fe6994adbda2d881f4be6fe3da7189264de14
Teslenko A.A.(work) <alteslo31@gmail.com>
2022-01-20 18:02:17 +0300

    fix

--- main.py
+++ main.py
@@ -13,19 +13,26 @@ def get_soup(url):
         print('Не удалось получить ссылку')
 
 
-inp_url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
-soup = get_soup(inp_url)
+def find_articles(soup):
+    inp_url = f'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page={page_num}'
+    soup = get_soup(inp_url)
+    tag_titles = soup.find_all('span', "c-meta__type")
+    for tag_title in tag_titles:
+        if 'type_articles' == tag_title.get_text():
+            title = tag_title.find_previous('h3').get_text().strip()
+            news_link = f"https://www.nature.com{tag_title.find_previous('a').get('href')}"
+            print(news_link)
+            news_soup = get_soup(news_link)
+            news_text = news_soup.find("div", "c-article-body").text.strip()
+            title = re.sub(r'''[^\w\s]''', '', title)
+            snake_title = title.replace(' ', '_')
+            with open(f'{snake_title}.txt', 'wb') as file:
+                file.write(news_text.encode())
+    print('Saved all articles.')
 
-tag_titles = soup.find_all('span', "c-meta__type")
 
-for tag_title in tag_titles:
-    if 'News' == tag_title.get_text():
-        title = tag_title.find_previous('h3').get_text().strip()
-        news_link = f"https://www.nature.com{tag_title.find_previous('a').get('href')}"
-        print(news_link)
-        news_soup = get_soup(news_link)
-        news_text = news_soup.find("div", "c-article-body").text.strip()
-        title = re.sub(r'''[^\w\s]''', '', title)
-        snake_title = title.replace(' ', '_')
-        with open(f'{snake_title}.txt', 'wb') as file:
-            file.write(news_text.encode())
+page_num = int(input())
+type_articles = input()
+
+
+find_articles()
