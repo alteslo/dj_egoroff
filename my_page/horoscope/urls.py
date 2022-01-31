5c01029d45c363bb3a65a5c69b2289692e9da11d
Teslenko A.A.(work) <alteslo31@gmail.com>
2022-01-31 15:47:26 +0300

    new routs in horoscope

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('<sign_zodiac>', views.get_info_about_zodiac_sign),
-    path('type/<type>', views.get_info_about_zodiac_types)
+    path('<int:month>/<int:day>', views.get_info_about_my_zodiac),
+    path('type/', views.get_info_about_zodiac_types, name='horo-type'),
+    path('type/<element>', views.get_info_about_element_zodiac),
+    path('<sign_zodiac>', views.get_info_about_zodiac_sign, name='info-sign')
 ]
--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from collections import namedtuple
 
 
 Type_zodiac = namedtuple('Type_zodiac', 'type zodiacs')
 fire = Type_zodiac('fire', ['aries', 'leo', 'sagittarius'])
-print(fire.zodiacs)
+earth = Type_zodiac('earth', ['taurus', 'virgo', 'capricorn'])
+air = Type_zodiac('air', ['gemini', 'libra', 'aquarius'])
+water = Type_zodiac('water', ['cancer', 'scorpio', 'pisces'])
+
+elements = [fire, earth, air, water]
 
 
 # Create your views here.
@@ -35,8 +41,55 @@ def get_info_about_zodiac_sign(request, sign_zodiac: str):
     if sign:
         return HttpResponse(sign)
     else:
-        return HttpResponseNotFound(f'Такого знака ({sign_zodiac}) нет на ')
+        return HttpResponseNotFound(f'Такого знака ({sign_zodiac}) нет')
 
 
-def get_info_about_zodiac_types(request, type):
-    pass
+def get_info_about_zodiac_types(request):
+    elements_url_li = ''
+    for element in elements:
+        elements_url_li += f'<li><a href="{element.type}">{element.type.capitalize()}</a></li>'
+    elements_url = f'<ul>{elements_url_li}</ul>'
+    return(HttpResponse(elements_url))
+
+
+def get_info_about_element_zodiac(request, element):
+    dict_elements = {el.type: el.zodiacs for el in elements}
+    element_zodiacs = dict_elements.get(element)
+    url_li = ''
+    if element_zodiacs is not None:
+        for zodiac in element_zodiacs:
+            url = reverse("info-sign", args=(zodiac, ))
+            url_li += f'<li><a href="{url}">{zodiac.capitalize()}</a></li>'
+        return HttpResponse(f'<ul>{url_li}</ul>')
+    else:
+        return HttpResponseNotFound(f'Такого элемента {element} нет')
+
+
+def get_info_about_my_zodiac(request, month, day):
+    Zodiac = namedtuple('Zodiacs', 'date sign')
+    zodiac_info = {
+        (3, 4): Zodiac({3: (21, 31), 4: (1, 20)}, 'aries'),
+        (4, 5): Zodiac({4: (21, 30), 5: (1, 20)}, 'taurus'),
+        (5, 6): Zodiac({5: (21, 31), 6: (1, 21)}, 'gemini'),
+        (6, 7): Zodiac({6: (21, 30), 7: (1, 22)}, 'cancer'),
+        (7, 8): Zodiac({7: (21, 31), 8: (1, 22)}, 'leo'),
+        (8, 9): Zodiac({8: (21, 31), 9: (1, 22)}, 'virgo'),
+        (9, 10): Zodiac({9: (21, 30), 10: (1, 22)}, 'libra'),
+        (10, 11): Zodiac({10: (21, 31), 11: (1, 21)}, 'scorpio'),
+        (11, 12): Zodiac({11: (21, 30), 12: (1, 20)}, 'sagittarius'),
+        (12, 1): Zodiac({12: (21, 31), 1: (1, 19)}, 'capricorn'),
+        (1, 2): Zodiac({1: (21, 31), 2: (1, 19)}, 'aquarius'),
+        (2, 3): Zodiac({2: (21, 29), 3: (1, 20)}, 'pisces')
+    }
+    if month in range(1,13):
+        for monthes in zodiac_info.keys():
+            if month in monthes:
+                zodiac = zodiac_info.get(monthes)
+                first, last = zodiac.date.get(month)
+                if day in range(first, last + 1):
+                    redirect_url = reverse('info-sign', args=(zodiac.sign, ))
+                    return HttpResponseRedirect(redirect_url)
+                else:
+                    return HttpResponseNotFound(f'Проверь день {day}')
+    else:
+        return HttpResponseNotFound(f'Проверь месяц {month}')
